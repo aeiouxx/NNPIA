781121b97e91d1a2262cee17a931b9de6e1f3efd
aeiouxx <st55776@upce.cz>
2024-06-02 16:56:20 +0200

    Category fixes

--- backend/src/main/java/com/aeiouxx/semestralniprace/repository/CategoryRepository.java
+++ backend/src/main/java/com/aeiouxx/semestralniprace/repository/CategoryRepository.java
@@ -1,7 +1,12 @@
 package com.aeiouxx.semestralniprace.repository;
 
+import com.aeiouxx.semestralniprace.dto.CategorySummary;
 import com.aeiouxx.semestralniprace.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.swing.text.html.Option;
@@ -10,8 +15,19 @@ import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    List<Category> findByUserId(Long userId);
-    Optional<Category> findByUserIdAndId(Long userId, Long categoryId);
+
+    @Query("SELECT new com.aeiouxx.semestralniprace.dto.CategorySummary(c.name, COUNT(a) ,COUNT(ae)) " +
+            "FROM Category c LEFT JOIN Activity a ON c.id = a.category.id " +
+            "LEFT JOIN ActivityEntry ae ON a.id = ae.activity.id " +
+            "WHERE c.user.id = :userId " +
+            "AND (:filter IS NULL OR c.name LIKE %:filter%) " +
+            "GROUP BY c.name")
+    Page<CategorySummary> findCategorySummariesByUserId(Pageable pageable,
+                                                        @Param("userId") Long userId,
+                                                        @Param("filter") String filter);
+
+    Page<Category> findByUserId(Pageable pageable, Long userId);
     Optional<Category> findByUserIdAndName(Long userId, String name);
-    Optional<Category> deleteByUserIdAndName(Long userId, String name);
+
+    void deleteByNameAndUserId(String name, Long userId);
 }
